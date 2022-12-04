7acfc05fd6c71629d0187079de6f9b217807f400
Kaj <tassi100@mail.chapman.edu>
2022-12-04 09:57:42 -0800

    security change

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -5,8 +5,8 @@
 import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
-const API_BASE_URL = //'https://back-end-one-rust.vercel.app'
-process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
+const API_BASE_URL = 
+  process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
